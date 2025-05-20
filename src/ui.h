b4351c7e7d3028e133beb4ff2bf5215278b60ef4
Gilang <adityasumarna21@gmail.com>
2025-05-20 16:05:25 +0700

    Membuat adt ui.h

--- src/ui.h
+++ src/ui.h
@@ -1,4 +1,22 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+/*
+    IS: Terminal kosong
+    FS: Terminal menampilkan pilihan menu dan menanyakan pilihan user
+
+    Creator: Andi Putra
+*/
+void menu(); 
+
+/*
+    IS: 
+    FS: 
+
+    Creator:
+*/
+
+char* input_user();
+
+
 #endif // PLAYER_H
