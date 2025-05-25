05ac3b87450a6c46f1af3de1625abe1b6786865b
andiputraw <andiputraw925@gmail.com>
2025-05-25 12:08:57 +0700

    miniaudio

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-clang -Wall -Wextra -Werror -std=c99 -o main ./src/*.c -I ./src/ -I ./external/ -lole32 -lshell32 -luuid
+# clang -c -o miniaudio.o ./external/miniaudio.c -I ./external/
+
+time clang -Wall -Wextra -Werror -std=c99 -o main miniaudio.o ./src/*.c -I ./src/ -I ./external/ -lole32 -lshell32 -luuid
